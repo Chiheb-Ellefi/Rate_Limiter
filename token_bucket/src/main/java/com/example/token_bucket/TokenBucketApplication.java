dee9ca0abb6131ecb23bcaf34f1c23804926dcb3
chihebellefi <chihebellefi888@gmail.com>
2024-12-15 14:42:26 +0100

    Enabled Scheduling

--- token_bucket/src/main/java/com/example/token_bucket/TokenBucketApplication.java
+++ token_bucket/src/main/java/com/example/token_bucket/TokenBucketApplication.java
@@ -2,8 +2,10 @@ package com.example.token_bucket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TokenBucketApplication {
 
 	public static void main(String[] args) {
