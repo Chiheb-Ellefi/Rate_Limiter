c6fbcd3a88bd23798fe100d4102b9213b295c393
chihebellefi <chihebellefi888@gmail.com>
2024-12-19 12:45:41 +0100

    Enabled the aspect proxy .

---
+++ sliding_window_log/src/main/java/com/example/sliding_window_log/config/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.example.sliding_window_log.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class ProjectConfig {
+}
