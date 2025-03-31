6a4d828cb56cdec5b41c0e0977e359aa18de2007
chihebellefi <chihebellefi888@gmail.com>
2025-03-31 17:39:07 +0100

    update

---
+++ sliding_window_log/src/main/java/com/example/sliding_window_log/SlidingWindowLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.sliding_window_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlidingWindowLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlidingWindowLogApplication.class, args);
+	}
+
+}
